de0f318b3aae59a912c49dbe503a57eb667ae24b
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-12-01 16:04:01 -0700

    mocked up the body of the page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import "./Home.css"
+import Product from "../Product/Product"; 
 
 export default function Home() {
     return (
@@ -14,17 +15,60 @@ export default function Home() {
                 {/* Rows of Products */}
                 <div className="home_row">
                     {/* Product  */}
-                    {/* Product  */}
-                    {/* Product  */}
+                    <Product 
+                    title="All-new Echo Dot (4th Gen) | Smart speaker with Alexa | Charcoal"
+                    price={48.99}
+                    image="https://m.media-amazon.com/images/I/714Rq4k05UL._SL110_.jpg"
+                    rating={5}/> 
+
+                    {/* https://www.amazon.com/dp/B0043R48Z4/ref=cg_HHG20-Hub-Creative_1b1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-top-slot-6&pf_rd_r=250FEQ7MEX2AKEB4716P&pf_rd_t=0&pf_rd_p=3391987a-735e-411e-8b3e-7c21077cb4f7&pf_rd_i=gf-events--HHG20-Hub */}
+                    <Product 
+                    title="NEST Fragrances Classic Candle- Holiday , 8.1 oz (NEST01HL)"
+                    price={42.95}
+                    image="https://images-na.ssl-images-amazon.com/images/I/816TV-bGJ3L._AC_SX679_.jpg"
+                    rating={4}
+                    />
+                    
                 </div>
 
                 <div className="home_row">
                     {/* Product  */}
-                    {/* Product  */}
+                    
+                    {/* https://www.amazon.com/dp/B08C4LC7TG/ref=cg_HHG20-Hub-Creative_1c1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-top-slot-6&pf_rd_r=250FEQ7MEX2AKEB4716P&pf_rd_t=0&pf_rd_p=3391987a-735e-411e-8b3e-7c21077cb4f7&pf_rd_i=gf-events--HHG20-Hub */}
+                    <Product 
+                    title="iRobot Roomba i3+ (3550) Robot Vacuum with Automatic Dirt Disposal"
+                    price={599.99}
+                    image="https://images-na.ssl-images-amazon.com/images/I/51RKEg3dp1L._AC_SL1000_.jpg"
+                    rating={4}
+                    />
+
+                    {/* https://www.amazon.com/dp/B075H7Z5L8/ref=cm_gf_aasc_iaai_d_p0_qd0_6uc1H8dT9qT00f8VMzXX */}
+                    <Product 
+                    title="MyQ Smart Garage Door Opener Chamberlain MYQ-G0301"
+                    price={29.98}
+                    image="https://images-na.ssl-images-amazon.com/images/I/61EgKv96u2L._AC_SL1000_.jpg"
+                    rating={4}
+                    />
+
+                    {/* https://www.amazon.com/dp/B019HR91W4/ref=cm_gf_aasc_iaab_d_p0_qd0_783sfYVjpUurJOJupkt4 */}
+                    <Product 
+                    title="Ecolution Original Microwave Micro-Pop Popcorn Popper"
+                    price={12.99}
+                    image="https://images-na.ssl-images-amazon.com/images/I/81aTLT1IstL._AC_SL1500_.jpg"
+                    rating={5}
+                    />
                 </div>
 
                 <div className="home_row">
                     {/* Product  */}
+
+                    {/* https://www.amazon.com/dp/B084TM9VB3/ref=cm_gf_aasc_iaab_d_p0_qd0_A11tefmTYikMT0u8JMYD */}
+                    <Product 
+                    title="Goodful Premium Non-Stick Cookware Set"
+                    price={119.99}
+                    image="https://images-na.ssl-images-amazon.com/images/I/813psexGaBL._AC_SL1500_.jpg"
+                    rating={4}
+                    />
                 </div>
             </div>
         </div>
---
+++ src/components/Product/Product.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./Product.css"; 
+
+function Product({title, image, price, rating}) {
+    return (
+        <div className="product">
+
+            {/* INFO  */}
+            <div className="product_info">
+
+                {/* DETAILS */}
+                <p>{title}</p>
+
+                {/* PRICE  */}
+                <p className="product_price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+
+                {/* RATING */}
+                <div className="product_rating">
+                {/* <p> {"⭐".repeat(rating)}</p> */}
+                {Array(rating).fill().map((_, i) => (
+                <p>⭐</p>
+                ))}
+                </div>
+
+            </div>
+
+            {/* IMAGE OF PRODUCT  */}
+            <img src={image} alt="" />
+
+            {/* ADD TO CART BUTTON  */}
+            <button className="product_button">Add to Cart</button>
+        </div>
+    )
+}
+
+export default Product
