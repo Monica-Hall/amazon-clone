359c038b227a498186b95637767e8ddb9ab9a6c8
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-12-01 11:05:52 -0700

    initialized header  component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import {React} from "react";
 import './App.css';
+import Header from "./components/Header/Header"; 
+
 
 function App() {
   return (
     <div className="App">
-<h1>Amazon Clone</h1>
+      <h1>Amazon Clone</h1>
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,34 @@ function Header() {
         <div className="header">
             <img 
                 className="header__logo" 
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="" 
             />
 
             <div className="header__search">
                 <input 
                     className="header__searchInput" 
                     type="text"/>
+            </div>
+
+            <div className="header__nav">
+                {/* Option 1  */}
+                <div className="header__options">
+                    <span className="header__optionLineOne">Hello</span>
+
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+                {/* Option 2 */}
+                <div className="header__options">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+                {/* Option 3 */}
+                <div className="header__options">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+
             </div>
         </div>
     )
