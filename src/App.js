1c5ff3f77f6411e6669a611b5aefd15c06dc48ce
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-12-01 16:30:09 -0700

    file structure for checkout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {React} from "react";
 import './App.css';
 import Header from "./components/Header/Header"; 
 import Home from "./components/Home/Home"; 
+import Checkout from "./components/Checkout/Checkout"; 
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"; 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
 
           <Route exact path="/checkout">
             <Header />
-            <h1>Checkout</h1>
+            <Checkout />
           </Route>
 
         </Switch>
---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Checkout.css"; 
+
+export default function Checkout() {
+    return (
+        <div>
+            <h1>I am the checkout page</h1>
+        </div>
+    )
+}
