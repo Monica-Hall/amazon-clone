2b5e3da8b653392eb6586e7f0e32d459239d62f5
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-12-08 14:09:59 -0700

    minor style fix for echo dot

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,11 +15,12 @@ export default function Home() {
                 {/* Rows of Products */}
                 <div className="home_row">
                     {/* Product  */}
+                    {/* https://www.amazon.com/dp/B07XJ8C8F5/ref=ods_gw_dec_d_be_xpl_1_en?pf_rd_r=XY88PCR4RHG6T5N1R3Z7&pf_rd_p=8d258d31-9bdf-4910-b555-94a817670cf1 */}
                     <Product 
                     id="1"
                     title="All-new Echo Dot (4th Gen) | Smart speaker with Alexa | Charcoal"
-                    price={48.99}
-                    image="https://m.media-amazon.com/images/I/714Rq4k05UL._SL110_.jpg"
+                    price={29.99}
+                    image="https://images-na.ssl-images-amazon.com/images/I/714Rq4k05UL._AC_SL1000_.jpg"
                     rating={5}/> 
 
                     {/* https://www.amazon.com/dp/B0043R48Z4/ref=cg_HHG20-Hub-Creative_1b1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-top-slot-6&pf_rd_r=250FEQ7MEX2AKEB4716P&pf_rd_t=0&pf_rd_p=3391987a-735e-411e-8b3e-7c21077cb4f7&pf_rd_i=gf-events--HHG20-Hub */}
